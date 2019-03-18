f78ab28ee3acfc7880e25bbb65aff7921522c9bb
capitobenj <48660025+capitobenj@users.noreply.github.com>
2019-03-18 02:35:03 -0300

    Update tp1_1.cpp

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -4,9 +4,10 @@ int main(){
 	int test=1;
 	int *punt;
 	punt=&test;
-	printf("Contenido de la variable: %d\n", *punt);
-	printf("Direccion de memoria de la variable: %u\n", punt);
-	printf("Direccion de memoria del puntero: %u\n", punt);
+	printf("Contenido del puntero (o variable): %d\n", *punt);
+	printf("La direccion de memoria almacenada por el puntero: %u\n", punt);
+	printf("Direccion de memoria de la variable: %u\n", &test);// (2) y (3) Estoy guardando la dirección de memoria de la variable en el puntero por ende la dirección de la variable y la dirección almacenada en el puntero son los mismos.
+	printf("Direccion de memoria del puntero: %u\n", &punt);// (4) es diferente porque los punteros son considerados variables también y por ende necesitan una dirección en la memoria.
 	printf("Tamaño de la variable: %d\n", sizeof(test));
 
 	return 0;
